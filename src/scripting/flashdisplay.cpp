bf08d461b45ec57fa1687c5f6610ecba35946487
Antti Ajanki <antti.ajanki@iki.fi>
2011-07-19 18:20:20 +0300

    flowplayer hack: return root loaderInfo for all DisplayObjects
    This is obviously wrong, but flowplayer expects that loaderInfo
    property of both Preloader (main class of the SWF) and Launcher
    (non-main class) classes return the root loaderInfo.

--- src/scripting/flashdisplay.cpp
+++ src/scripting/flashdisplay.cpp
@@ -1549,11 +1549,13 @@ ASFUNCTIONBODY(DisplayObject,_constructor)
 ASFUNCTIONBODY(DisplayObject,_getLoaderInfo)
 {
 	DisplayObject* th=static_cast<DisplayObject*>(obj);
-	if(th->loaderInfo.isNull())
-		return new Undefined;
 
-	th->loaderInfo->incRef();
-	return th->loaderInfo.getPtr();
+	_NR<RootMovieClip> r=th->getRoot();
+	if(r.isNull() || r->loaderInfo.isNull())
+		return new Undefined;
+	
+	r->loaderInfo->incRef();
+	return r->loaderInfo.getPtr();
 }
 
 ASFUNCTIONBODY(DisplayObject,_getStage)
